fad4f7dc5bc3889d0dcee8c02c911774658c9360
timothyromanowski <timothyromanowski@192.168.1.106>
2010-07-15 13:33:22 -0700

    commiting

--- DoDah/gen/com/android/dodah/R.java
+++ DoDah/gen/com/android/dodah/R.java
@@ -14,15 +14,12 @@ public final class R {
         public static final int icon=0x7f020000;
     }
     public static final class id {
-        public static final int loginButton=0x7f050008;
-        public static final int widget33=0x7f050000;
-        public static final int widget37=0x7f050001;
-        public static final int widget57=0x7f050002;
-        public static final int widget58=0x7f050005;
-        public static final int widget59=0x7f050003;
-        public static final int widget60=0x7f050006;
-        public static final int widget61=0x7f050004;
-        public static final int widget63=0x7f050007;
+        public static final int login_button=0x7f050005;
+        public static final int password_text=0x7f050003;
+        public static final int txt_password=0x7f050004;
+        public static final int txt_username=0x7f050001;
+        public static final int username_text=0x7f050002;
+        public static final int welcome_text=0x7f050000;
     }
     public static final class layout {
         public static final int main=0x7f030000;
--- DoDah/src/com/android/dodah/Main.java
+++ DoDah/src/com/android/dodah/Main.java
@@ -1,28 +1,87 @@
 package com.android.dodah;
-
+ 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
-
-public class Main extends Activity implements OnClickListener {
-	
-	public void onClick(View v) {
-	      // do something when the button is clicked
-	   
-	}
-	
-	/** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+import android.widget.EditText;
+import android.content.DialogInterface;
+ 
+/**
+ *  Application Name: Generic Login Screen for the Android Platform (back end)
+ *  Description: This is a generic login screen which catches the username and password values
+ *  Created on: November 23, 2007
+ *  Created by: Pogz Ortile
+ *  Contact: pogz(at)redhat(dot)polarhome(dot)com
+ *  Notes: The string values for username and password are assigned to sUserName and sPassword respectively
+ *              You are free to distribute, modify, and wreck for all I care. GPL ya!
+ * */
+ 
+public class Main extends Activity {
+    /** Called when the activity is first created. */
+    @Override   
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
+       
+        // load up the layout
         setContentView(R.layout.main);
-        
-        Button button = (Button)findViewById(R.id.loginButton);
-        // Register the onClick listener with the implementation above
-        button.setOnClickListener(this);
-        
-        
+       
+        // get the button resource in the xml file and assign it to a local variable of type Button
+        Button launch = (Button)findViewById(R.id.login_button);
+       
+        // this is the action listener
+        launch.setOnClickListener( new OnClickListener()
+        {
+               
+                public void onClick(View viewParam)
+                {
+                        // this gets the resources in the xml file and assigns it to a local variable of type EditText
+                EditText usernameEditText = (EditText) findViewById(R.id.txt_username);
+                EditText passwordEditText = (EditText) findViewById(R.id.txt_password);
+               
+                // the getText() gets the current value of the text box
+                // the toString() converts the value to String data type
+                // then assigns it to a variable of type String
+                String sUserName = usernameEditText.getText().toString();
+                String sPassword = passwordEditText.getText().toString();
+                       
+                // this just catches the error if the program cant locate the GUI stuff
+                if(usernameEditText == null || passwordEditText == null){
+                    showAlert("Crap!", "Couldn't find the 'txt_username' or 'txt_password' "
+                              + "EditView in main.xml");
+                }else{
+                        // display the username and the password in string format
+                        showAlert("Logging in", "Username: " + sUserName + "nPassword: " + sPassword);
+                        }
+                }
+        }
+       
+        ); // end of launch.setOnclickListener
     }
+    
+   
+    
+    public void showAlert( String title, String message )
+    {
+        new AlertDialog.Builder(this)
+          .setMessage(message)
+          .setTitle(title)
+          .setPositiveButton("ok", new DialogInterface.OnClickListener() {
+			public void onClick(DialogInterface dialog, int which) {
+				// TODO Auto-generated method stub
+				
+			}})
+          .setCancelable(true)
+          .setOnCancelListener(new DialogInterface.OnCancelListener() {
+            public void onCancel(DialogInterface dialog) {
+            	// TODO Auto-generated method stub
+            }})
+          .show();
+    }
+    
 }
